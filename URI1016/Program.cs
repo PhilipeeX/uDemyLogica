cb01bd1f06ac310901d054517af5a70e9c3f44d0
PhilipeeX <dev.philipe@outlook.com>
2022-03-12 09:36:09 -0300

    exercicio 1016 aceito

---
+++ URI1016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace URI1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo, distancia;
+            distancia = int.Parse(Console.ReadLine());
+
+            tempo = distancia * 2;
+            Console.WriteLine(tempo + " minutos");
+        }
+    }
+}
