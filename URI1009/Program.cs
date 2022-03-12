a748ab24c793b03bdf6331949d76ddbb0bc0cc80
PhilipeeX <dev.philipe@outlook.com>
2022-03-12 07:50:30 -0300

    exercício 1009 aceito

---
+++ URI1009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace URI1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario,vendas,totalSalario;
+            string nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            totalSalario = vendas * 0.15 + salario;
+            Console.WriteLine("TOTAL = R$ " + totalSalario.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
