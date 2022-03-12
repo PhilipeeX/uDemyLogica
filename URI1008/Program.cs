52b26a301b64425deddf05183e9d902445e073da
PhilipeeX <dev.philipe@outlook.com>
2022-03-12 07:42:08 -0300

    exercício 1008 ACEITO

---
+++ URI1008/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace URI1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nFuncionario, horas;
+            double pHora, totalHora;
+            
+            nFuncionario = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            pHora = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            totalHora = (double)pHora * horas;
+
+            Console.WriteLine("NUMBER = " + nFuncionario);
+            Console.WriteLine("SALARY = U$ " + totalHora.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
