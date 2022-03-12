e62dcf6b305b30be5babc019b96ce5fcfeaf6d41
PhilipeeX <dev.philipe@outlook.com>
2022-03-12 08:16:33 -0300

    exercicio 1014 aceito

---
+++ URI1014/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace URI1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y,mediaKm;
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            mediaKm = X / Y;
+
+            Console.WriteLine(mediaKm.ToString("F3",CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
