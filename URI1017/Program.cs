9b5e05b59ab764310b2170eacdaa960d8e31c83e
PhilipeeX <dev.philipe@outlook.com>
2022-03-14 19:56:15 -0300

    exercicio 1017 aceito

---
+++ URI1017/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace URI1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoTotal, veloc, kmTotal;
+            double litros;
+
+            tempoTotal = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            veloc = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            kmTotal = tempoTotal * veloc;
+
+            litros = (double)kmTotal / 12;
+
+            Console.WriteLine(litros.ToString("F3",CultureInfo.InvariantCulture));
+        }
+    }
+}
