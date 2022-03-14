6061c12b27c508095695902dfc940d754cf4fa5f
PhilipeeX <dev.philipe@outlook.com>
2022-03-14 20:28:30 -0300

    exercicio 1006 aceito

---
+++ URI1006/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+namespace URI1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C,peso,media;
+            peso = 10;
+            A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            media = (A * 2 + B * 3 + C * 5) / peso;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1",CultureInfo.InvariantCulture));
+        }
+    }
+}
