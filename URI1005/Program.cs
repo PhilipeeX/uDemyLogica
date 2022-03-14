5d0cd40b1e68e1d1f4fb042f49fce64830fefe01
PhilipeeX <dev.philipe@outlook.com>
2022-03-14 20:12:31 -0300

    exercicio 1005 aceito

---
+++ URI1005/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace URI1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B,peso,media;
+            peso = 11;
+            A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            media = ((A * 3.5) + (B * 7.5)) / peso;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5",CultureInfo.InvariantCulture));
+        }
+    }
+}
