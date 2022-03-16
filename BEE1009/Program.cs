5cbe0b2b5289b5b1c205ccf7a4450922bc242636
PhilipeeX <dev.philipe@outlook.com>
2022-03-16 20:20:30 -0300

    Exercício 1009 aceito

---
+++ BEE1009/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+namespace BEE1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi,volume,raio;
+            pi = 3.14159;
+            raio = double.Parse(Console.ReadLine());
+            
+            volume = (4.0 / 3.0) * pi * Math.Pow(raio,3.0);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3",CultureInfo.InvariantCulture));
+        }
+    }
+}
