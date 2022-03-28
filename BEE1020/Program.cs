9f406746f76a12d8a552c593dad8837871fc7187
Philipe <dev.philipe@outlook.com>
2022-03-28 18:40:36 -0300

    Exercício 1020 aceito

---
+++ BEE1020/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BEE1020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,anos,dias,meses;
+            N = int.Parse(Console.ReadLine());
+
+            anos = N / 365;
+            meses = N % 365 / 30;
+            dias = N % 365 % 30;
+
+
+            Console.WriteLine($"{anos} ano(s)");
+            Console.WriteLine($"{meses} mes(es)");
+            Console.WriteLine($"{dias} dia(s)");
+        }
+    }
+}
