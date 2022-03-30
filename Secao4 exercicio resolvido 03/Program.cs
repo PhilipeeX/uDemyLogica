0ae69f1e812ea81c66382ec0c88e428a1249e875
PhilipeeX <dev.philipe@outlook.com>
2022-03-30 13:00:09 -0300

    Seção 4 exercício 03 resolvido

---
+++ Secao4 exercicio resolvido 03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Secao4_exercicio_resolvido_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            C = int.Parse(vet[2]);
+
+            if (A <= B && A <= C) {
+                Console.WriteLine($"MENOR = {A}");
+            } else if (B <= A && B <= C) {
+                Console.WriteLine($"MENOR = {B}");
+            } else if (C <= A && C <=B ) {
+                Console.WriteLine($"MENOR = {C}");
+            }
+        }
+    }
+}
