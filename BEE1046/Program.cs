ea865524db89ec4a4189c45f5c2bad8acf82fa0a
PhilipeeX <dev.philipe@outlook.com>
2022-03-31 11:10:49 -0300

    Exercício 1046 aceito

---
+++ BEE1046/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BEE1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, total;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+
+            if (A < B)
+            {
+                total = B - A;
+                Console.WriteLine($"O JOGO DUROU {total} HORA(S)");
+            }
+            else if (A > B) {
+                total = (24 - A) + B;
+                Console.WriteLine($"O JOGO DUROU {total} HORA(S)");
+            }
+            else { 
+                Console.WriteLine($"O JOGO DUROU 24 HORA(S)"); }
+        }
+    }
+}
