960aa3deed0ec68eb780f88c981ce89c245cb726
PhilipeeX <dev.philipe@outlook.com>
2022-04-06 08:40:12 -0300

    Exercício 1047 não aceito, porém resolvido(melhorar a sintaxe do código)

---
+++ BEE1047/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BEE1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hInicial, hFinal, mInicial, mFinal, horas, minutos;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            hInicial = int.Parse(vet[0]);
+            mInicial = int.Parse(vet[1]);
+            hFinal = int.Parse(vet[2]);
+            mFinal = int.Parse(vet[3]);
+
+            if (hInicial < hFinal && mInicial <= mFinal)
+            {
+                horas = hFinal - hInicial;
+                minutos = mFinal - mInicial;
+
+            }
+            else if (hInicial < hFinal && mInicial > mFinal)
+            {
+                horas = hFinal - hInicial - 1;
+                minutos = (60 - mInicial) + mFinal;
+            }
+            else if (hInicial > hFinal && mInicial <= mFinal)
+            {
+                horas = (24 - hInicial) + hFinal;
+                minutos = mFinal - mInicial;
+            }
+            else if (hInicial > hFinal && mInicial > mFinal) 
+            {
+                horas = (24 - hInicial) + hFinal - 1;
+                minutos = (60 - mInicial) + mFinal; ;
+            }
+            else
+            {
+                horas = 24;
+                minutos = 0;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {horas} HORAS E {minutos} MINUTOS.");
+        }
+    }
+}
