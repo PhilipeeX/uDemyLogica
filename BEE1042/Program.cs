f3cfeaac71e944e45f5e9acfd94cbe9a87a6f039
PhilipeeX <dev.philipe@outlook.com>
2022-04-14 10:41:20 -0300

    Exercício 1051 inciado, porém não finalizado

---
+++ BEE1042/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BEE1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c,maiorAB,maiorBC,maior,meio,menor;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+            maiorAB = (a + b + Math.Abs(a - b)) / 2;
+            maiorBC = (b + c + Math.Abs(b - c))/2;
+
+            if (a >= maiorBC)
+            {
+                maior = a;
+                meio = maiorBC;
+            }
+            else if (c >= maiorAB)
+            {
+                maior = c;
+                meio = maiorAB;
+            }
+            else { Console.WriteLine("seila"); }
+
+            Console.WriteLine($"{maior} {meio}");
+            
+        }
+    }
+}
