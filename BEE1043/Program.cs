3a04d4220e645ccb5a0753d588e398a52d5511f6
PhilipeeX <dev.philipe@outlook.com>
2022-05-08 11:32:17 -0300

    Exercício 1043 aceito

---
+++ BEE1043/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace BEE1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c,moduloBC,moduloAC,moduloAB,perimetro,area;
+            string[] vet = Console.ReadLine().Split(' ');
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            moduloBC = Math.Abs(b-c);
+            moduloAC = Math.Abs(a-c);
+            moduloAB = Math.Abs(a-b);
+
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                perimetro = a + b + c;
+                Console.WriteLine($"Perimetro = {perimetro.ToString("F1",CultureInfo.InvariantCulture)}");
+            }
+            else 
+            {
+                area = ((a + b) / 2) * c;
+                Console.WriteLine($"Area = {area.ToString("F1",CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
