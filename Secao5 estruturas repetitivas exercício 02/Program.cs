f72e5bd04e9903955666dfa590d54fef405cda43
PhilipeeX <dev.philipe@outlook.com>
2022-05-25 14:10:16 -0300

    Nao terminado(rever)

--- Secao5 estruturas repetitivas exercício 02/Program.cs
+++ Secao5 estruturas repetitivas exercício 02/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Globalization;
 namespace Secao5_estruturas_repetitivas_exercício_02
 {
     class Program
@@ -10,21 +10,27 @@ namespace Secao5_estruturas_repetitivas_exercício_02
             // O último dado que não entrará no cálculo, contém um valor de idade negativa.Calcule e imprima a idade
             // média deste grupo de indivíduos. Se for entrado um valor negativo na primeira vez mostre a mensagem
             // "impossível calcular".
-            int x = 0;
-            double media,mediaFinal;
-            double idade = double.Parse(Console.ReadLine());
+
+            double media;
+            double idade = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double x = 0.0;
+            double soma = 0.0;
+
+            while (idade > 0)
+            {
+                x += 1;
+                soma = soma + idade;
+                idade = double.Parse(Console.ReadLine());
+            }
             if (idade < 0)
             {
-                Console.WriteLine($"Impossivel calcular");
+                Console.WriteLine($"Impossível Calcular");
             }
             else
             {
-                while (idade > 0)
-                {
-                    x += 1;
-                    media = idade / x;
-                    idade = double.Parse(Console.ReadLine());
-                }
+                media = soma / x;
+                Console.WriteLine($"{media.ToString("F2", CultureInfo.InvariantCulture)}");
             }
         }
     }
