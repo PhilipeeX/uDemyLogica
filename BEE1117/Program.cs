b1944e559e0dcd79785e3eba2b75f7337132af69
Philipe <dev.philipe@outlook.com>
2022-06-01 19:48:47 -0300

    Exercício BEE1117 Aceito

---
+++ BEE1117/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace BEE1117
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double media;
+
+            while (nota1 < 0 || nota1 >10) {
+                Console.WriteLine("nota invalida");
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (nota2 <0 || nota2 > 10) {
+                Console.WriteLine("nota invalida");
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            media = (nota1 + nota2) / 2;
+            Console.WriteLine($"media = {media.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
