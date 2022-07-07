773bb22941c61e7f39ce94c4daa756275c228603
Philipe <dev.philipe@outlook.com>
2022-07-07 09:51:19 -0300

    1113 e 1114 dnv

---
+++ BEE1113(denovo)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BEE1113_denovo_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+            string[] vet = Console.ReadLine().Split(' ');
+            X = int.Parse(vet[0]);
+            Y = int.Parse(vet[1]);
+
+            while (X != Y) {
+                if (X > Y)
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                else { Console.WriteLine("Crescente"); 
+                }
+                vet = Console.ReadLine().Split(' ');
+                X = int.Parse(vet[0]);
+                Y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
