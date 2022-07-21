d65499263891806aaeaaaecb5f45fe25ec93d1a8
PhilipeeX <dev.philipe@outlook.com>
2022-07-21 13:22:44 -0300

    BEE1131 não aceito(Presentation error(100%), procurar o erro*

---
+++ BEE1131/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace BEE1131
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, grenais, empates, novo, inter, gremio;
+
+            string[] vet; 
+            novo = 1;
+            grenais = 0;
+            inter = 0;
+            gremio = 0;
+            empates = 0;
+
+            while (novo == 1)
+            {
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+                grenais += 1;
+                if (x > y)
+                {
+                    inter += 1;
+                }
+                else if (x < y)
+                {
+                    gremio += 1;
+                }
+                else
+                {
+                    empates += 1;
+                }
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                novo = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"{grenais} grenais");
+            Console.WriteLine($"Inter: {inter}");
+            Console.WriteLine($"Gremio: {gremio}");
+            Console.WriteLine($"Empates: {empates}");
+            if (gremio > inter)
+            {
+                Console.WriteLine("Gremio venceu mais");
+            }
+            else if (inter > gremio)
+            {
+                Console.WriteLine("Inter venceu mais");
+            }
+            else {
+                Console.WriteLine("Não houve vencedor");
+            }
+        }
+    }
+}
---
+++ BEE1159/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace BEE1159
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soma de Pares Consecutivos
+
+            double soma, x,y;
+            x = int.Parse(Console.ReadLine());
+            soma = 0;
+
+            while (x != 0)
+            {
+                if (x % 2 == 0)
+                {
+                    soma = x + (x+2) + (x+4) + (x+6) + (x+8);
+                }
+                else
+                {
+                    y = x + 1;
+                    soma = y + (y + 2) + (y + 4) + (y + 6) + (y + 8);
+                }
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{soma}");
+            }
+        }
+    }
+}
