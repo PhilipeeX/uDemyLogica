4371977d8b09b43b61e7fdc25b3d0116b9c0a6db
PhilipeeX <dev.philipe@outlook.com>
2022-07-22 15:40:53 -0300

    Exercício 1079 aceito

---
+++ BEE1079/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace BEE1079
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            float x, y, z,media;
+            
+            for (int i=0; i<N;i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                x = float.Parse(vet[0], CultureInfo.InvariantCulture);
+                y = float.Parse(vet[1], CultureInfo.InvariantCulture);
+                z = float.Parse(vet[2], CultureInfo.InvariantCulture);
+
+                x = x * 2f;
+                y = y * 3f;
+                z = z * 5f;
+                media = (x + y + z) / 10;
+                Console.WriteLine($"{media.ToString("F1",CultureInfo.InvariantCulture)}");
+            }
+
+        }
+    }
+}
