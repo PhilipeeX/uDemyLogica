4b22ee510b9c03df468b996bbb5df4cfeac1a7be
PhilipeeX <dev.philipe@outlook.com>
2022-07-22 09:18:04 -0300

    Exercício 1071 e 1078

---
+++ BEE1071/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BEE1071
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = ;
+            int soma = 0;
+
+            for (int i = 1; z < ((x+y+Math.Abs(x-y))/2); i++) {
+                z = x + i;
+                if (z % 2 == 0)
+                {
+                    z = z + 1;
+                }
+                else {
+                    soma = soma + z;
+                }
+            }
+
+        }
+    }
+}
---
+++ BEE1078/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BEE1078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int result;
+
+            for (int i = 1; i <= 10 ; i++ ) {
+                result = N * i;
+                Console.WriteLine($"{i} x {N} = {result}");
+            }
+        }
+    }
+}
