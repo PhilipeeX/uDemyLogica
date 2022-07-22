d977c85f8c6acfeb61378f0b29544a172f8e2251
PhilipeeX <dev.philipe@outlook.com>
2022-07-22 12:42:55 -0300

    Exercício 1073 aceito

---
+++ BEE1073/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BEE1073
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            double quadrado = 0;
+
+            for (int i =2; i <= N; i=i+2) {
+                quadrado = Math.Pow(i, 2.0);
+                Console.WriteLine($"{i}^2 = {quadrado}");
+            }
+        }
+    }
+}
