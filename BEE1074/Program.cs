9c08ef18e985cff348799fc14e6c603fc053edce
PhilipeeX <dev.philipe@outlook.com>
2022-07-22 13:07:25 -0300

    Exercício 1074 aceito

---
+++ BEE1074/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BEE1074
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int valor;
+
+            for (int i = 0; i < N; i++) {
+                valor = int.Parse(Console.ReadLine());
+                if (valor == 0) {
+                    Console.WriteLine("NULL");
+                } else if (valor % 2 == 0) {
+                    Console.Write($"EVEN ");
+                } else {
+                    Console.Write($"ODD ");
+                }
+                if (valor > 0)
+                {
+                    Console.WriteLine("POSITIVE");
+                }
+                else if (valor < 0)
+                {
+                    Console.WriteLine("NEGATIVE");
+                }
+                else { }
+                }
+            }
+        }
+    }
