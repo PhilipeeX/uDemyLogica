638721fc9f92c7f2b7ff4a65959866a489ec9ab0
PhilipeeX <dev.philipe@outlook.com>
2022-07-25 08:14:01 -0300

    Exercício 1080 não aceito

---
+++ BEE1080/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BEE1080
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, ultimoN,posicao,maior;
+            ultimoN = 0;
+            maior = 0;
+            posicao = 0;
+
+            for (int i=1; i<=100;i++) {
+                N = int.Parse(Console.ReadLine());
+                if (N > ultimoN)
+                {
+                    maior = N;
+                    posicao = i;
+                }
+                else
+                {
+                    ultimoN = N;
+                }
+            }
+            Console.WriteLine($"{maior}");
+            Console.WriteLine($"{posicao}");
+        }
+    }
+}
