91333a7e728e5b0e4dda47ecde531512ef10796b
PhilipeeX <dev.philipe@outlook.com>
2022-07-25 14:00:59 -0300

    Exercício 1142 aceito

---
+++ BEE1142/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BEE1142
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<=(N*4);i++) {
+                if (i % 4 == 0) {
+                    Console.WriteLine("PUM");
+                }
+                else
+                {
+                    Console.Write($"{i} ");
+                }
+            
+            }
+        }
+    }
+}
