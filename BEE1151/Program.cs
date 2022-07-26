992163c04bf151b845bb09e65b55b40ced17b7dd
PhilipeeX <dev.philipe@outlook.com>
2022-07-26 14:57:12 -0300

    Exercício 1151 não resolvido, Exercício 1153 aceito

---
+++ BEE1151/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BEE1151
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int x;
+
+
+            for (int i =1; i<=N; i++) {
+                
+            }
+        }
+    }
+}
---
+++ BEE1153/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BEE1153
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int fatorial = N;
+
+            for (int i = N-1; i>=1; i=i-1) {
+                fatorial = fatorial * i;
+            }
+            Console.WriteLine($"{fatorial}");
+        }
+    }
+}
