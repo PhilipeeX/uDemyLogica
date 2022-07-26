a57217765af9641137cdcf6f42299cd981b63f6f
PhilipeeX <dev.philipe@outlook.com>
2022-07-26 09:21:12 -0300

    Exercício 1143 aceito

---
+++ BEE1143/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BEE1143
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<=N; i++) {
+                double qua = (double)Math.Pow(i, 2.0);
+                double cub = (double)Math.Pow(i, 3.0);
+
+                Console.WriteLine($"{i} {qua} {cub}");
+            }
+        }
+    }
+}
