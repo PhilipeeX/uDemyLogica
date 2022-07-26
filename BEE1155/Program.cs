e6f857ba63c2ad647b900b070c1d9b9f14f2dc2a
PhilipeeX <dev.philipe@outlook.com>
2022-07-26 15:57:58 -0300

    Exercicio 1155 aceito

---
+++ BEE1155/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BEE1155
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double S = 1;
+            for (double i = 2.0; i <= 100; i++)
+            {
+                S = S + (1.0/i);
+            }
+            Console.WriteLine($"{S.ToString("F2")}");
+        }
+    }
+}
