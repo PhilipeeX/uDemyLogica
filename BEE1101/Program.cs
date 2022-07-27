8a585f084c64f6cf0f33b3301699eacdebae8da5
PhilipeeX <dev.philipe@outlook.com>
2022-07-27 09:32:13 -0300

    Exercício 1101 aceito

---
+++ BEE1101/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BEE1101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menor, maior, soma; ;
+            menor = 0;
+            maior = 0;
+            
+            string[] vet = Console.ReadLine().Split(' ');
+            int M = int.Parse(vet[0]);
+            int N = int.Parse(vet[1]);
+
+
+            while (M > 0 && N >0) {
+
+                soma = 0;
+                if (M > N) {
+                    maior = M;
+                    menor = N;
+                } else if (N>M) {
+                    maior = N;
+                    menor = M;
+                }
+                for (int i = menor; i<=maior;i++) {
+                    soma = soma + i;
+                    Console.Write($"{i} ");
+                    if (i==maior) {
+                        Console.WriteLine($"Sum={soma}");
+                    }
+                }
+
+
+                vet = Console.ReadLine().Split(' ');
+                M = int.Parse(vet[0]);
+                N = int.Parse(vet[1]);
+            }
+        }
+    }
+}
