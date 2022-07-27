c9fa8cb7a1985999f6c6c917ce24a383047cd63f
PhilipeeX <dev.philipe@outlook.com>
2022-07-27 10:28:11 -0300

    Exercício 1145 aceito

---
+++ BEE1145/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BEE1145
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escreva um programa que leia dois valores X e Y.
+            // A seguir, mostre uma sequência de 1 até Y, passando
+            // para a próxima linha a cada X números.
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int X = int.Parse(vet[0]);
+            int y = int.Parse(vet[1]);
+
+            for (int i = 1; i <= y; i++)
+            {
+                if (i % X == 0)
+                {
+                    Console.WriteLine($"{i}");
+                }
+                else if (i == y)
+                {
+                    Console.Write($"{i}");
+                }
+                else
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
