4c452b6e5d984193d4ea2e47b454da0caf17a1ac
PhilipeeX <dev.philipe@outlook.com>
2022-07-27 12:43:22 -0300

    Exercício 1150 aceito

---
+++ BEE1150/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BEE1150
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int Z = int.Parse(Console.ReadLine());
+            while (Z <= X)
+            {
+                Z = int.Parse(Console.ReadLine());
+            }
+            int soma = X;
+            int i = 0;
+
+            for (i = 1; soma <= Z; i++)
+            {
+                soma = soma + (X + i);
+            }
+            Console.WriteLine($"{i}");
+        }
+
+    }
+}
+
