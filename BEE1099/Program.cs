a73a5bdf48fd2fb4b6a5d0e5a80502cff366185b
PhilipeeX <dev.philipe@outlook.com>
2022-07-27 09:15:21 -0300

    Exercício 1099 aceito

---
+++ BEE1099/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BEE1099
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int maior, menor,soma;
+            menor = 0;
+            maior = 0;
+            soma = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                int x = int.Parse(vet[0]);
+                int y = int.Parse(vet[1]);
+
+                if (x > y)
+                {
+                    maior = x;
+                    menor = y;
+                }
+                else if (y > x)
+                {
+                    maior = y;
+                    menor = x;
+                }
+                for (int I = menor+1; I<maior; I++) {
+                    if (I % 2 != 0) {
+                        soma += I;
+                    }
+                }
+                Console.WriteLine($"{soma}");
+                soma = 0;
+                maior = 0;
+                menor = 0;
+            }
+        }
+    }
+}
