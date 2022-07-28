bba15e5becbada0d9d8d82c9e9760cc7a1603105
PhilipeeX <dev.philipe@outlook.com>
2022-07-28 12:20:48 -0300

    vetores exercício 01 feito

---
+++ secao6aula5 vetores exercicio 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace secao6aula5_vetores_exercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um programa que leia N números inteiros earmazene-os em um vetor. Em seguida mostrar na tela todos 
+            // os números negativos lidos.
+
+            int[] vet;
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i =0;i<N;i++) {
+                vet[i] = int.Parse(s[i]);
+            }
+            for (int i=0;i<N;i++) {
+                if (vet[i]<0) {
+                    Console.WriteLine($"{vet[i]}");
+                }
+            }
+        }
+    }
+}
