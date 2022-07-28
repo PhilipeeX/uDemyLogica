d3c7ded42daea22fb606a7e2a661967ab68ce84f
PhilipeeX <dev.philipe@outlook.com>
2022-07-28 14:54:28 -0300

    exercicio 02 e 03 de vetores resolvido

---
+++ secao6aula6 exercício resolvido 02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+namespace secao6aula6_exercício_resolvido_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um programa que leia N números reais e armazene-os em um vetor.Em seguida:
+            // - imprimir todos os elementos do vetor
+            // - Mostrar na tela a soma e a média dos elementos do vetor.
+
+            int N;
+            double[] vet;
+            double soma = 0;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+            string[] vet2 = Console.ReadLine().Split(' ');
+            for (int i =0;i<N;i++) {
+                vet[i] = double.Parse(vet2[i],CultureInfo.InvariantCulture);
+                soma = soma + vet[i];
+            }
+            for (int i =0;i<N;i++) {
+                Console.Write($"{vet[i].ToString("F1",CultureInfo.InvariantCulture)} ");
+                if (i==N-1) {
+                    Console.WriteLine($"{vet[i].ToString("F1", CultureInfo.InvariantCulture)} ");
+                }
+            }
+            double media = soma / N;
+            Console.WriteLine($"{soma.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"{media.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ secao6aula7 vetores exercicio resolvido 03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace secao6aula7_vetores_exercicio_resolvido_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string nome;
+            int idade,menor,porcentagemMenor;
+            double altura,alturaMedia,somaAltura;
+            somaAltura = 0;
+            menor = 0;
+            for (int i =0;i<N;i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                nome = vet[0];
+                idade = int.Parse(vet[1]);
+                altura = double.Parse(vet[2],CultureInfo.InvariantCulture);
+                somaAltura += altura;
+                if (idade<16) {
+                    menor++;
+                }
+            }
+            alturaMedia = somaAltura / N;
+            porcentagemMenor = (menor *100) /N;
+            Console.WriteLine($"Altura média: {alturaMedia.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Pessoas com menos de 16 anos: {porcentagemMenor.ToString("F1",CultureInfo.InvariantCulture)}%");
+        }
+    }
+}
