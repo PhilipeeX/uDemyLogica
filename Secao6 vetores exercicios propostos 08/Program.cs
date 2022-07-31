c9987213f1823fff7825945a90c848a9c3852069
PhilipeeX <dev.philipe@outlook.com>
2022-07-31 17:12:25 -0300

    exercicio proposto 09 aceito

---
+++ Secao6 vetores exercicios propostos 08/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+namespace Secao6_vetores_exercicios_propostos_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tem-se um conjunto de dados contendo a altura e o sexo (M, F) de N pessoas. Fazer um programa que calcule e escreva:
+            //- a maior e a menor altura do grupo
+            //- a média de altura das mulheres
+            //- o número de homens
+
+            int N = int.Parse(Console.ReadLine());
+            double[] altura = new double[N];
+            char[] sexo = new char[N];
+
+            for (int i =0; i<N;i++) {
+                string[] dados = Console.ReadLine().Split(' ');
+                altura[i] = double.Parse(dados[0],CultureInfo.InvariantCulture);
+                sexo[i] = char.Parse(dados[1]);
+            }
+            //maior e menor altura
+            double maiorA = altura[0];
+            double menorA = altura[0];
+            for (int i = 1; i<N;i++) {
+                if (altura[i]>maiorA) {
+                    maiorA = altura[i];
+                } else if (altura[i]<menorA) {
+                    menorA = altura[i];
+                }
+            }
+            // media da altura das mulheres e total homens
+            double somaAlturasM = 0;
+            int totalMulheres = 0;
+            int totalHomens = 0;
+            for (int i =0;i<N;i++) {
+                if (sexo[i] == 'F')
+                {
+                    somaAlturasM += altura[i];
+                    totalMulheres += 1;
+                }
+                else { totalHomens += 1; }
+            }
+            double mediaAlturaMulheres = somaAlturasM / totalMulheres;
+
+            Console.WriteLine($"Menor altura = {menorA.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Maior altura = {maiorA.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Media das alturas das mulheres = {mediaAlturaMulheres.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Numero de homens = {totalHomens}");
+
+        }
+    }
+}
---
+++ secao6 vetores exercicios propostos 09/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+namespace secao6_vetores_exercicios_propostos_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[N];
+            double[] precoC = new double[N];
+            double[] precoV = new double[N];
+            int abaixoDeDez = 0;
+            int entreDezEVinte =0;
+            int acimaDeVinte =0;
+            double valorTotalDeCompra = 0;
+            double valorTotalDeVenda = 0;
+            double lucroTotal;
+
+            for (int i =0; i<N;i++) {
+                string[] dados = Console.ReadLine().Split(' ');
+                nomes[i] = dados[0];
+                precoC[i] = double.Parse(dados[1],CultureInfo.InvariantCulture);
+                precoV[i] = double.Parse(dados[2],CultureInfo.InvariantCulture);
+                valorTotalDeCompra += precoC[i];
+                valorTotalDeVenda += precoV[i];
+            }
+            lucroTotal = valorTotalDeVenda - valorTotalDeCompra;
+
+            for (int i =0;i<N;i++) {
+                double dezPorcento = precoC[i] / 10.0;
+                double vintePorcento = (precoC[i] / 10.0) *2.0;
+                if (precoV[i] - precoC[i] < dezPorcento) {
+                    abaixoDeDez ++;
+                } else if (precoV[i] - precoC[i] >= dezPorcento && precoV[i] - precoC[i] <= vintePorcento) {
+                    entreDezEVinte ++;
+                } else if (precoV[i] - precoC[i] > vintePorcento) {
+                    acimaDeVinte++;
+                }
+            }
+            Console.WriteLine($"Lucro abaixo de 10%: {abaixoDeDez}");
+            Console.WriteLine($"Lucro entre 10% e 20%: {entreDezEVinte}");
+            Console.WriteLine($"Lucro acima de 20%: {acimaDeVinte}");
+            Console.WriteLine($"Valor total de compra: {valorTotalDeCompra.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor total de venda: {valorTotalDeVenda.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Lucro total: {lucroTotal.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
