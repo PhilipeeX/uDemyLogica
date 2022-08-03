35fc947bc5368a2abf82fe04774ff4e7a5a275ea
PhilipeeX <dev.philipe@outlook.com>
2022-08-03 08:27:31 -0300

    matrizes

---
+++ secao7 Matrizes exericios propostos 04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace secao7_Matrizes_exericios_propostos_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[,] mat = new int[N, N];
+
+            for (int i =0; i<N; i++) {
+                string[] dados = Console.ReadLine().Split(' ');
+                for (int j =0; j<N; j++) {
+                    mat[i, j] = int.Parse(dados[j]);
+                }
+            }
+            int soma = 0;
+            for (int i = 0; i<N; i++) {
+                for (int j = i+1; j<N; j++) {
+                    soma = soma+mat[i,j];
+                }
+            }
+            Console.WriteLine($"{soma}");
+        }
+    }
+}
---
+++ secao7 matrizes exercicios propostos 03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace secao7_matrizes_exercicios_propostos_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[,] mat = new int[N, N];
+
+            for (int i =0;i<N;i++) {
+                string[] dados = Console.ReadLine().Split(' ');
+                for (int j =0; j< N; j++) {
+                    mat[i, j] =int.Parse(dados[j]);
+                }
+            }
+            int[] maior = new int[N];
+            int mais = 0;
+            for (int i =0; i<N; i++) {
+                for (int j =0; j<N; j++) {
+                    if (mat[i,j]>maior[i]) {
+                        maior[i] = mat[i,j];
+                    }
+                }
+                mais++;
+            }
+            for (int i =0; i<N; i++) {
+                Console.WriteLine($"{maior[i]}");
+            }
+        }
+    }
+}
