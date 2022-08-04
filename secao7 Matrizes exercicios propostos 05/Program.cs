ab68ac89c7a682245ac1a78357ae29abf7ddb8a0
PhilipeeX <dev.philipe@outlook.com>
2022-08-04 17:32:12 -0300

    exercicio 05 matriz

---
+++ secao7 Matrizes exercicios propostos 05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace secao7_Matrizes_exercicios_propostos_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mN = Console.ReadLine().Split(' ');
+            int M = int.Parse(mN[0]);
+            int N = int.Parse(mN[1]);
+
+            int[,] A = new int[M, N];
+            int[,] B = new int[M, N];
+            int[,] C = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] dados = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = int.Parse(dados[j]);
+                }
+            }
+            for (int i = 0; i < M; i++)
+            {
+                string[] dados = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    B[i, j] = int.Parse(dados[j]);
+                }
+            }
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    C[i, j] = A[i, j] + B[i, j];
+                    Console.Write($"{C[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
